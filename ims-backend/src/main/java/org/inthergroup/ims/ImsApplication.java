816456ff3d6377cee49a54c067bb5f7517c95dc0
ionatamanenco <101548435+ionatamanenco@users.noreply.github.com>
2022-03-15 15:32:55 +0200

    fixed errors

--- ims-backend/src/main/java/org/inthergroup/ims/ImsApplication.java
+++ ims-backend/src/main/java/org/inthergroup/ims/ImsApplication.java
@@ -1,4 +1,6 @@
+package org.inthergroup.ims;
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
