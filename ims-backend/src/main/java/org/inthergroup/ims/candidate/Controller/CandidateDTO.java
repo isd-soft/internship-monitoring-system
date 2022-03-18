249da3fe3af663415fabcf67a7cb23f12292e9ce
BerezovskiAlex <berezovski789@gmail.com>
2022-03-18 15:37:26 +0200

    fixed entity and added base64 func

--- ims-backend/src/main/java/org/inthergroup/ims/candidate/Controller/CandidateDTO.java
+++ ims-backend/src/main/java/org/inthergroup/ims/candidate/Controller/CandidateDTO.java
@@ -11,6 +11,6 @@ public class CandidateDTO {
     private String email;
     private String cv;
     private String comment;
-    private String status;
+    private Status status;
     private double mark;
 }
--- ims-backend/src/main/java/org/inthergroup/ims/candidate/Service/CandidateService.java
+++ ims-backend/src/main/java/org/inthergroup/ims/candidate/Service/CandidateService.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Service
 public interface CandidateService {
 
-    List<Candidate> get();
+    List<Candidate> getAllCandidates();
 
     void save(Candidate candidate);
 
