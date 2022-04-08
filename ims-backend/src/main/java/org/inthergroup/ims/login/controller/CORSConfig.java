bfbe760832127f578ec38b3f90f7621dd5db496e
ionatamanenco <atamanenco.ion99@gmail.com>
2022-04-08 10:53:46 +0300

    deploy try

--- ims-backend/src/main/java/org/inthergroup/ims/login/controller/CORSConfig.java
+++ ims-backend/src/main/java/org/inthergroup/ims/login/controller/CORSConfig.java
@@ -9,6 +9,8 @@ public class CORSConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("*");
     }
 }
